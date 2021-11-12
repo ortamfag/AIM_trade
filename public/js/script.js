5d9a8ac53649b8b77bfe5246d7d83cfe04fd6fd4
ortamfag <a_zhidov@outlook.com>
2021-11-12 19:42:17 +0300

    ADD:analysis block

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,32 @@
-const g2020 = document.querySelector('.g2020');
-const g2021 = document.querySelector('.g2021');
-const g2025 = document.querySelector('.g2025');
+const firstlist = document.querySelector('.list1');
+const secondlist = document.querySelector('.list2');
+const thirdlist = document.querySelector('.list3');
 
-const burger = document.querySelector('.y2020');
-const y2021 = document.querySelector('.y2021');
-const y2025 = document.querySelector('.y2025');
+const first = document.querySelector('.year2020');
+const second = document.querySelector('.year2021');
+const third = document.querySelector('.year2025');
 
 
-burger.addEventListener('click', () => y2020.classList.toggle('analysis__inner-itemgreen'));
-burger.addEventListener('click', () => g2020.classList.toggle('g2020'));
+first.addEventListener('click', () => first.classList.toggle('analysis__inner-itemgreen1'));
+first.addEventListener('click', () => second.classList.remove('analysis__inner-itemgreen2'));
+first.addEventListener('click', () => third.classList.remove('analysis__inner-itemgreen3'));
+//
+first.addEventListener('click', () => firstlist.classList.toggle('list1active'));
+first.addEventListener('click', () => secondlist.classList.remove('list2active'));
+first.addEventListener('click', () => thirdlist.classList.remove('list3active'));
 
-y2021.addEventListener('click', () => y2021.classList.toggle('analysis__inner-itemgreen'));
-y2021.addEventListener('click', () => g2021.classList.toggle('g2021'));
+second.addEventListener('click', () => second.classList.toggle('analysis__inner-itemgreen2'));
+second.addEventListener('click', () => first.classList.remove('analysis__inner-itemgreen1'));
+second.addEventListener('click', () => third.classList.remove('analysis__inner-itemgreen3'));
+//
+second.addEventListener('click', () => secondlist.classList.toggle('list2active'));
+second.addEventListener('click', () => firstlist.classList.remove('list1active'));
+second.addEventListener('click', () => thirdlist.classList.remove('list3active'));
 
-y2025.addEventListener('click', () => y2025.classList.toggle('analysis__inner-itemgreen'));
-y2025.addEventListener('click', () => g2025.classList.toggle('g2025'));
+third.addEventListener('click', () => third.classList.toggle('analysis__inner-itemgreen3'));
+third.addEventListener('click', () => first.classList.remove('analysis__inner-itemgreen1'));
+third.addEventListener('click', () => second.classList.remove('analysis__inner-itemgreen2'));
+//
+third.addEventListener('click', () => thirdlist.classList.toggle('list3active'));
+third.addEventListener('click', () => firstlist.classList.remove('list1active'));
+third.addEventListener('click', () => secondlist.classList.remove('list2active'));
