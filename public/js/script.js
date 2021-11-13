548b447ca120701ed45fcbeb4672d994d5c20277
ortamfag <a_zhidov@outlook.com>
2021-11-13 04:07:00 +0300

    ADD: popups

--- public/js/script.js
+++ public/js/script.js
@@ -30,3 +30,25 @@ third.addEventListener('click', () => second.classList.remove('analysis__inner-i
 third.addEventListener('click', () => thirdlist.classList.toggle('list3active'));
 third.addEventListener('click', () => firstlist.classList.remove('list1active'));
 third.addEventListener('click', () => secondlist.classList.remove('list2active'));
+
+
+// modals 
+const makeModal = modalSel => {
+	const modalEl = document.querySelector(modalSel);
+		btnEl = document.querySelector(modalSel + '-btn'),
+		closeEl = document.querySelector(modalSel + ' .modal-wrapper__bg'),
+
+	btnEl.addEventListener('click', () => modalEl.classList.add('active'));
+	closeEl.addEventListener('click', () => modalEl.classList.remove('active'));
+}
+
+makeModal('#modal-1');
+makeModal('#modal-2');
+makeModal('#modal-3');
+makeModal('#modal-4');
+makeModal('#modal-5');
+makeModal('#modal-6');
+makeModal('#modal-7');
+// end modals
+
+
